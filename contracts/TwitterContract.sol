375e28429c172acc76a4551762d05f9d051e1d0b
omkarg01 <omkargujja01@gmail.com>
2023-01-04 22:36:43 +0530

    getAllFunc commented

--- contracts/TwitterContract.sol
+++ contracts/TwitterContract.sol
@@ -5,10 +5,38 @@ contract TwitterContract{
 
     // create a struct Tweet field id, address, tweetText, isDeleted
 
+    // create a event AddTweet takes takes address and tweetId
+
     // a private arr DS for storing all tweets
 
     // a map of tweet id to owner address
 
+
+    // a func called addTweet takes tweetText and isDeleted (this func will called from frontend - externally)
+    {
+        // set tweetId
+
+        // creat a Tweet Object and push it in tweets
+
+        // map tweetId with its owner address
+
+        // emit the AddTweet event
+
+    }
+
+
+    // a func called getAllTweets
+    {
+        // create a temp memory for Tweet[] of len tweets.length
+
+        // start counter
+        // store all the tweet which has isDeleted as false in temp memory
+
+        // create a result memory for Tweet[] of len counter
+        // store all tweets from temp to result
+
+        // return result;
+    }
 
     
 

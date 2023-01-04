15fe1ac78a7b9a0e96c3eaf4a9a4597b5daf762e
omkarg01 <omkargujja01@gmail.com>
2023-01-04 22:15:37 +0530

    comments for twitter contract

--- contracts/TwitterContract.sol
+++ contracts/TwitterContract.sol
@@ -1,3 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+contract TwitterContract{
+
+    // create a struct Tweet field id, address, tweetText, isDeleted
+
+    // a private arr DS for storing all tweets
+
+    // a map of tweet id to owner address
+
+
+    
+
+}

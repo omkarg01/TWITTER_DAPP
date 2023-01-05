2bf539035f41fce081949de4d84066b740b35ade
omkarg01 <omkargujja01@gmail.com>
2023-01-05 21:11:40 +0530

    addTweet done

--- contracts/TwitterContract.sol
+++ contracts/TwitterContract.sol
@@ -4,24 +4,38 @@ pragma solidity ^0.8.9;
 contract TwitterContract{
 
     // create a struct Tweet field id, address, tweetText, isDeleted
+    struct Tweet {
+        uint tweetId;
+        address username;
+        string tweetText;
+        bool isDeleted;
+    }
 
-    // create a event AddTweet takes takes address and tweetId
+    // create a event AddTweet takes address and tweetId
+    event AddTweet(address recipient, uint tweetId);
+    event DeleteTweet(uint tweetId, bool isDeleted);
 
     // a private arr DS for storing all tweets
+    Tweet[] private tweets;
 
     // a map of tweet id to owner address
+    mapping (uint => address) tweetToOwner;
 
 
     // a func called addTweet takes tweetText and isDeleted (this func will called from frontend - externally)
-    {
+    function addTweet(string memory tweetText, bool isDeleted) externally {
         // set tweetId
+        uint tweetId = tweets.length;
 
         // creat a Tweet Object and push it in tweets
+        Tweet tweet = Tweet(tweetId, msg.sender, tweetText, isDeleted);
+        tweets.push(tweet);
 
         // map tweetId with its owner address
+        tweetToOwner[tweetId] = msg.sender;
 
         // emit the AddTweet event
-
+        emit AddTweet(msg.sender, tweetId);
     }
 
 
